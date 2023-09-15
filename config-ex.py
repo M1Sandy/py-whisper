e59f744f0e2c5d24ac9f6550d3dfb52e32c539b7
sandy <m1s4ndy@gmail.com>
2023-09-15 04:39:37 +0300

    adding config file

---
+++ config-ex.py
@@ -0,0 +1,7 @@
+webhookport = 8888
+device = "GPU" # "" for CPU             ## you need to install cuda or whatever for AMD
+model = "medium"    # large # base
+libretranslate = "libretranslate_IP"
+target_languge = "ar"   # target lang to translate to after whisper produce its subtitle
+src_language = "en"     
+subtitle_custom_ext = "sandy"   # this to generate multiple vers of the subtitle
